87454f612a5c9c89fe03ecfcc354dd3f09cf47cd
Dan <dan.meierotto@gmail.com>
2012-10-20 09:21:10 -0400

    added new files

---
+++ app.js
@@ -0,0 +1,40 @@
+// *******************************************************
+// expressjs template
+//
+// assumes: npm install express
+// defaults to jade engine, install others as needed
+//
+// assumes these subfolders:
+//   public/
+//   public/javascripts/
+//   public/stylesheets/
+//   views/
+//
+var express = require('express');
+var app = express();
+var viewEngine = 'jade'; // modify for your view engine
+// Configuration
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', viewEngine);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+// *******************************************************
+
+app.get('/survey', function(req, res) {
+    res.render('index');
+});
+
+//start the http server
+app.listen(process.env.C9_PORT);
+
+
