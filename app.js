ce260b50af754f2f8cc22ff2afc3d7b7ad147f2f
Dan <dan.meierotto@gmail.com>
2012-10-20 10:25:00 -0400

    finished template

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
-// *******************************************************
-// expressjs template
-//
-// assumes: npm install express
-// defaults to jade engine, install others as needed
-//
-// assumes these subfolders:
-//   public/
-//   public/javascripts/
-//   public/stylesheets/
-//   views/
-//
+
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
+
 var app = express();
-var viewEngine = 'jade'; // modify for your view engine
+
 // Configuration
+
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', viewEngine);
+  app.set('view engine', 'jade');
+  app.set('view options', { layout: false }); 
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-// *******************************************************
 
-app.get('/survey', function(req, res) {
-    res.render('index');
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+app.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact', {
+    title: 'Contact'
+  });
 });
 
 //start the http server
